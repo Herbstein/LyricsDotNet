955fb041d629010ea38326b54c9619adcc4599df
Rasmus Larsen <doctorherbstein@gmail.com>
2017-01-22 03:11:49 +0100

    Slightly better handling of errors

--- LyricsDotNet/MainWindow.xaml.cs
+++ LyricsDotNet/MainWindow.xaml.cs
@@ -53,43 +53,48 @@
             var friendlyArtist = UrlFriendlyString(track.ArtistResource.Name);
 
             using (var client = new WebClient()) {
-                var html =
-                    client.DownloadString(new Uri($"http://azlyrics.com/lyrics/{friendlyArtist}/{friendlyTrack}.html"));
-                var split = html.Split(
-                                       new[] {
-                                           "<!-- Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that. -->"
-                                       },
-                                       StringSplitOptions.None);
-                var splitHtml = split[1];
-
-                split = splitHtml.Split(
-                                         new[] {
-                                             "</div>"
-                                         },
-                                         StringSplitOptions.None);
-                var lyrics = split[0];
-
-                lyrics = Regex.Replace(lyrics, "(<.*?>)", "");
-                lyrics = Regex.Replace(lyrics, "&quot;|&#34;|&#x22;|&ldquo;|&#147;|&#x93;|&rdquo;|&#148;|&#x94", "\"");
-                lyrics = Regex.Replace(lyrics, "&amp;|&#38;|&#x26;", "&");
-                lyrics = Regex.Replace(lyrics, "&lsquo;|&#145;|&#x91;|&#rsquo;|&#146;|&#x92;|&#39;|&#x27;", "'");
-                lyrics = Regex.Replace(lyrics, "&#40;|&#x28;", "(");
-                lyrics = Regex.Replace(lyrics, "&#41;|&#x29;", ")");
-                lyrics = Regex.Replace(lyrics, "&#33;|&#x21;", "!");
-                lyrics = Regex.Replace(lyrics, "&#42;|&#x2a;", "*");
-                lyrics = Regex.Replace(lyrics, "&#44;|&#x2c;", ",");
-                lyrics = Regex.Replace(lyrics, "&ndash;|&#150;|&#x96;|&mdash;|&#151;|&#x97;|&#45;|&#x2d;", "-");
-                lyrics = Regex.Replace(lyrics, "&#46;|&#x2e;", ".");
-                lyrics = Regex.Replace(lyrics, "&#59;|&#x3b;", ";");
-                lyrics = Regex.Replace(lyrics, "&#63;|&#x3f;", "?");
-                lyrics = Regex.Replace(lyrics, "&#64;|&#x40;", "@");
-                lyrics = Regex.Replace(lyrics, "&#35;|&#x23;", "#");
-                lyrics = Regex.Replace(lyrics, "&#91;|&#x5b;", "[");
-                lyrics = Regex.Replace(lyrics, "&trade;|&#153;|&#x99;", "TM");
-
-                lyrics = lyrics.Trim();
-
-                Lyrics.Text = lyrics;
+                try {
+                    var html =
+                        client.DownloadString(
+                                              new Uri(
+                                                      $"http://azlyrics.com/lyrics/{friendlyArtist}/{friendlyTrack}.html"));
+                    var split = html.Split(
+                                           new[] {
+                                               "<!-- Usage of azlyrics.com content by any third-party lyrics provider is prohibited by our licensing agreement. Sorry about that. -->"
+                                           },
+                                           StringSplitOptions.None);
+                    var splitHtml = split[1];
+                    split = splitHtml.Split(
+                                            new[] {
+                                                "</div>"
+                                            },
+                                            StringSplitOptions.None);
+                    var lyrics = split[0];
+                    lyrics = Regex.Replace(lyrics, "(<.*?>)", "");
+                    lyrics = Regex.Replace(
+                                           lyrics,
+                                           "&quot;|&#34;|&#x22;|&ldquo;|&#147;|&#x93;|&rdquo;|&#148;|&#x94",
+                                           "\"");
+                    lyrics = Regex.Replace(lyrics, "&amp;|&#38;|&#x26;", "&");
+                    lyrics = Regex.Replace(lyrics, "&lsquo;|&#145;|&#x91;|&#rsquo;|&#146;|&#x92;|&#39;|&#x27;", "'");
+                    lyrics = Regex.Replace(lyrics, "&#40;|&#x28;", "(");
+                    lyrics = Regex.Replace(lyrics, "&#41;|&#x29;", ")");
+                    lyrics = Regex.Replace(lyrics, "&#33;|&#x21;", "!");
+                    lyrics = Regex.Replace(lyrics, "&#42;|&#x2a;", "*");
+                    lyrics = Regex.Replace(lyrics, "&#44;|&#x2c;", ",");
+                    lyrics = Regex.Replace(lyrics, "&ndash;|&#150;|&#x96;|&mdash;|&#151;|&#x97;|&#45;|&#x2d;", "-");
+                    lyrics = Regex.Replace(lyrics, "&#46;|&#x2e;", ".");
+                    lyrics = Regex.Replace(lyrics, "&#59;|&#x3b;", ";");
+                    lyrics = Regex.Replace(lyrics, "&#63;|&#x3f;", "?");
+                    lyrics = Regex.Replace(lyrics, "&#64;|&#x40;", "@");
+                    lyrics = Regex.Replace(lyrics, "&#35;|&#x23;", "#");
+                    lyrics = Regex.Replace(lyrics, "&#91;|&#x5b;", "[");
+                    lyrics = Regex.Replace(lyrics, "&trade;|&#153;|&#x99;", "TM");
+                    lyrics = lyrics.Trim();
+                    Lyrics.Text = lyrics;
+                } catch (Exception e) {
+                    Lyrics.Text = "No lyrics could be found for this song. Sorry!";
+                }
             }
         }
     }
